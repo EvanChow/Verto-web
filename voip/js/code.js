e21c87670960c82485408519f12f12b4f8c8dc53
EvanChow <evanzhou1992@163.com>
2016-06-03 16:44:59 +0800

    Add files via upload

---
+++ voip/js/code.js
@@ -0,0 +1,30 @@
+/*  
+ @Name: 编码
+ @Author：Evan
+ @Desc：FreeSwiCH问题集合
+ @Date: 2016-04-17
+ @QQ: 1349090004
+ @Email: evanzhou1992@163.com
+ */
+
+var FreeSwitchCode={
+UNSPECIFIED:{id:0,code:0,enumeration:"UNSPECIFIED",
+	cause:"",
+	description_en:"",
+	description_cn:""},
+UNSPECIFIED:{id:0,code:0,enumeration:"UNSPECIFIED",
+	cause:"Unspecified. No other cause codes applicable",
+	description_en:"This is usually given by the router when none of the other codes apply. This cause usually occurs in the same type of situations as cause 1, cause 88, and cause 100.",
+	description_cn:"这通常是由路由器当没有其他的规范应用.导致该原因的情况：cause 1, cause 88, and cause 100."},
+UNALLOCATED_NUMBER:{id:1,code:404,enumeration:"UNALLOCATED_NUMBER",
+	cause:"Unallocated (unassigned) number",
+	description_en:"",
+	description_cn:"号码是空号"},
+NO_ROUTE_TRANSIT_NET:{id:2,code:404,enumeration:"",
+	cause:"",
+	description_en:"",
+	description_cn:"网络不通"},
+	
+	
+	
+};
